e9f6cacfac7d0822bc7d5d194dcbf757855c64b4
Kensuke_Mitsui <mitsui.investment@gmail.com>
2020-05-06 16:27:29 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/master.key }
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -26,13 +26,13 @@ namespace :deploy do
     invoke 'unicorn:start'
   end
 
-  desc 'upload master.key'
+  desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
   before :starting, 'deploy:upload'
