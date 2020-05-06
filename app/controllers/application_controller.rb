2e2811ab30cd9f99d1c9b577f411a948b01215e0
Kensuke_Mitsui <mitsui.investment@gmail.com>
2020-05-06 20:07:36 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth　if: :production?
+  before_action :basic_auth
 
   private
-  def production?
-    Rails.env.production?
-  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-
+      username == 'admin' && password == '2222'
     end
   end
 end
