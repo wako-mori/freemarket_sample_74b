d5d487edab215a3fea7e265887d083cb73e009a1
Kensuke_Mitsui <mitsui.investment@gmail.com>
2020-05-06 16:31:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.0"
 
 set :application, "freemarket_sample_74b"
-set :repo_url,  'Kensuke-Mitsui/freemarket_sample_74b.git'
+set :repo_url,  'git@github.com:Kensuke-Mitsui/freemarket_sample_74b.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
