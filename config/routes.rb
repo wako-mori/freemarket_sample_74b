ba14594a1e3bb562ca1d66a35adc58d08ae8350e
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-05-09 17:37:36 +0900

    データ登録時に削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'creditcard/new'
-
-  get 'orders/index'
+  get 'creditcard/new'  #users情報を登録した後に削除
+  get 'orders/index'    #items情報を登録した後に削除
 
   root 'items#index'
 
