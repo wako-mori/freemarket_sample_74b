7955f873587f87168be832eeee74bea33afe10ab
tomoya <f.t.1998@i.softbank.jp>
2020-05-11 16:39:17 +0900

    mapage 実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,9 @@
 class UsersController < ApplicationController
-  
-  def index
+
+  def show
   end
 
   def new
   end
-
-  def show
-  end
   
-  def create
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  private
-  def aaa
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'creditcard/new'  #users情報を登録した後に削除
-  get 'orders/index'    #items情報を登録した後に削除
-
+  get 'creditcard/new'  
+  get 'orders/index'   
+  get 'users/show'
   root 'items#index'
 
   resources :items do
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :user do
+  resources :users, only: [:index, :new] do
     resources :creditcard, only: [:new] do
     end
   end
