bb3febc42d01b12cd2348d074564a45ad21cdbec
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-05-12 12:56:11 +0900

    credit_cardマイグレーション修正

--- db/migrate/20200507060721_create_credit_cards.rb
+++ db/migrate/20200507060721_create_credit_cards.rb
@@ -1,11 +1,9 @@
 class CreateCreditCards < ActiveRecord::Migration[5.0]
   def change
     create_table :credit_cards do |t|
-      t.integer	:card_number,	null: false
-      t.integer	:expiration_year,	null: false
-      t.integer	:expiration_month,	null: false
-      t.integer	:security,	null: false
-      t.references	:user,	null: false, foreign_key: true
+      t.integer :user_id, null: false
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
 
       t.timestamps
     end
