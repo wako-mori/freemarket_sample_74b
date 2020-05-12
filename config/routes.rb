d0fc10f6f9be45e3d2a00c965b5b07fbebd74672
tomoya <f.t.1998@i.softbank.jp>
2020-05-12 10:47:53 +0900

    routes.rb・改行の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'creditcard/new'  
   get 'orders/index'   
   get 'users/show'
-  
+  # get 'creditcard/new'  #users情報を登録した後に削除
+  # get 'orders/index'    #items情報を登録した後に削除
 
   root 'items#index'
   devise_for :users
