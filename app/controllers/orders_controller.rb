b78947dd793a8051f62ca256068a519177468c6c
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-05-15 13:30:04 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,31 @@
 class OrdersController < ApplicationController
+  require 'payjp'
+
+
   def index
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to controller: "creditcard", action: "new"
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      customer =Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
+
+  def pay
+    card = Card.where(user_id: current_user.id).first
+    item = Item.find(params[:item_id])
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+      amount: item.price,
+      customer: card.customer_id,
+      currency: 'jpy',
+    )
+  redirect_to action: 'done'
+  end
+
+  def done
+  end
+  
 end
