251ab922bab865e4df87aceeaa396b71c9fceef7
kosukekiga <kigakiga0919@gmail.com>
2020-05-18 15:12:29 +0900

    商品出品機能(バリデーション)
    ◼︎画像が0枚の時に出品できないようになっている
    ◼︎「必須」と表現してあるカラムが空のときに出品ができない
    ◼︎「必須」と表現していないカラムが空のときには出品ができる
    ◼︎その他バリデーションが正しく設けられている

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20200516050558) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -24,12 +23,6 @@ ActiveRecord::Schema.define(version: 20200516050558) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
-  end
-
-  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
