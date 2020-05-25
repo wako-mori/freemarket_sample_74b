e7f4c8d645c41741795c7826bfd49c8792bfd154
ohnitakahiro <ohnitakahiro@gmail.com>
2020-05-25 22:10:31 +0900

    不要な記述削除

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,6 +2,8 @@ $(document).on('turbolinks:load',(function(){
   $(function(){
     var index = [1,2,3,4,5,6,7,8,9,10];
     var loc = location.pathname;
+    // 入力フォームの変形
+    
     // 新規出品
     if( loc.indexOf("new") != -1){
       console.log(index);
@@ -87,14 +89,12 @@ $(document).on('turbolinks:load',(function(){
       }else if(element_length == 10){
         $(".new-wrapper__main__image-field").css("display","none");
       }else{
-        $(".new-wrapper__main__image-field").css("width",(10 - element_length) *124);
+        $(".new-wrapper__main__image-field").css("width",124);
       }     
       index.shift();
       // 既に使われているindexの削除
       lastIndex = $('.new-wrapper__main__preview__image__img:last').data('index');
-      console.log(lastIndex);
       index.splice(0, lastIndex - 1);
-      console.log(index);
       $('.hidden-destroy').hide();
 
       // 追加機能
@@ -108,29 +108,10 @@ $(document).on('turbolinks:load',(function(){
           $(".flexbox").append(`<input class="file-field" type="file" name="item[images_attributes][${index[1]}][image]" id="item_images_attributes_${index[1]}_image">`);
           $("#image-wrapper").attr("for",`item_images_attributes_${index[1]}_image`);
           index.shift();
-
           if(index.length > 5){
-            $("#image-field-second").remove();
             $(".new-wrapper__main__image-field").css("display","flex");
-            $(".new-wrapper__main__image-field").css("width",(index.length-5)*124);
-          }else if(index.length == 5){
-            $(".new-wrapper__main__image-field").css("display","none");
-            $("#image-wrapper").append(`
-              <div class="new-wrapper__main__image-field" id="image-field-second">
-                <i class="fas fa-camera"></i>
-                <div class="new-wrapper__main__image-field__text">
-                  ドラッグアンドドロップ
-                  <br>
-                  またはクリックしてファイルをアップロード
-                </div>
-              </div>
-            `);
-            $(".new-wrapper__main__preview").attr("class", "new-wrapper__main__preview-first");
-            $(".new-wrapper__main__preview-first").after(`<div class="new-wrapper__main__preview"></div>`);
-          }else if(index.length == 0){
-            $("#image-field-second").css("display","none");
+            $(".new-wrapper__main__image-field").css("width",(index.length-5)*124);           
           }
-          $("#image-field-second").css("width",index.length*124);
         }
       }
 
@@ -138,15 +119,11 @@ $(document).on('turbolinks:load',(function(){
       // 削除機能
       $(".flexbox").on("click", ".delete-btn", function(){
         const targetIndex = $(this).parent().data('index')
-        console.log(targetIndex);
         // 該当indexを振られているチェックボックスを取得する
         const hiddenCheck = $(`input[data-index="${targetIndex - 1}"].hidden-destroy`);
         // もしチェックボックスが存在すればチェックを入れる
         if (hiddenCheck) hiddenCheck.prop('checked', true);
         index.push(targetIndex);
-        console.log(index);
-        console.log($(this).parent().parent().attr("class"));
-
         if($(this).parent().parent().attr("class") == "new-wrapper__main__preview-first"){
           $(".new-wrapper__main__preview .new-wrapper__main__preview__image:first").appendTo(".new-wrapper__main__preview-first");
         }
